86fe879e83a5b8b19d5fe6393aac873788a5f3fe
Armando <armando.ortiz1@upr.edu>
2018-05-12 11:39:49 +0900

    migration to ionic

--- README.md
+++
@@ -1,4 +0,0 @@
-# Key Writer
-Write music for your piano anywhere you go!   
-  
-Smartphone application to write visual piano music  
---
+++ src/pages/home/home.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {calculateNotes, NotesInterface} from '../../assets/data/calculate-notes';
+import {initializeSegments, SegmentsInterface} from '../../assets/data/note-segment';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+	notes: NotesInterface[];
+	segments: SegmentsInterface[];
+	whiteBottomKeys: NotesInterface[];
+	currentSegmentIndex: number;
+
+  constructor() {
+		this.notes = calculateNotes();
+		this.segments = initializeSegments(this.notes);
+		this.whiteBottomKeys = this.notes.filter(n => n.key.slice(-1) !== '#');
+		this.currentSegmentIndex = 0;
+  }
+	playNote(note: NotesInterface){
+		let isPlaying = !this.segments[this.currentSegmentIndex].noteToggles[note.id-1];
+		this.segments[this.currentSegmentIndex].noteToggles[note.id-1] = isPlaying;
+		if (isPlaying){
+			//play frequency audio
+		}
+		return;
+  }
+
+  selectSegment(index: number){
+    this.currentSegmentIndex = index;
+    this.playSegment(index);
+    return;
+  }
+
+  playSegment(segmentIndex: number){
+  	//play all note frequencies according to this.segments[segmentIndex].noteToggles
+  	// for a duration of this.segments[segmentIndex].duration according to defined BPM
+    return segmentIndex;
+  }
+
+}
