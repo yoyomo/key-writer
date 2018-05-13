8988dcb857af6e9c5dbfda0c0322c24687db2e5f
Armando <armando.ortiz1@upr.edu>
2018-05-13 15:23:54 +0900

    scroll to bottom on load

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {calculateNotes, NotesInterface} from '../../assets/data/calculate-notes';
 import {initializeSegments, SegmentsInterface} from '../../assets/data/note-segment';
 
@@ -7,35 +7,44 @@ import {initializeSegments, SegmentsInterface} from '../../assets/data/note-segm
   templateUrl: 'home.html'
 })
 export class HomePage {
-	notes: NotesInterface[];
-	segments: SegmentsInterface[];
-	whiteBottomKeys: NotesInterface[];
-	currentSegmentIndex: number;
+  notes: NotesInterface[];
+  segments: SegmentsInterface[];
+  whiteBottomKeys: NotesInterface[];
+  currentSegmentIndex: number;
+
+  //scrolls to bottom whenever the page has loaded
+  ionViewDidEnter() {
+    let screen = document.getElementById("screen");
+    if (screen) {
+      screen.scrollTop = screen.scrollHeight;
+    }
+  }
 
   constructor() {
-		this.notes = calculateNotes();
-		this.segments = initializeSegments(this.notes);
-		this.whiteBottomKeys = this.notes.filter(n => n.key.slice(-1) !== '#');
-		this.currentSegmentIndex = 0;
-  }
-	playNote(note: NotesInterface){
-		let isPlaying = !this.segments[this.currentSegmentIndex].noteToggles[note.id-1];
-		this.segments[this.currentSegmentIndex].noteToggles[note.id-1] = isPlaying;
-		if (isPlaying){
-			//play frequency audio
-		}
-		return;
+    this.notes = calculateNotes();
+    this.segments = initializeSegments(this.notes);
+    this.whiteBottomKeys = this.notes.filter(n => n.key.slice(-1) !== '#');
+    this.currentSegmentIndex = 0;
   }
 
-  selectSegment(index: number){
+  playNote(note: NotesInterface) {
+    let isPlaying = !this.segments[this.currentSegmentIndex].noteToggles[note.id - 1];
+    this.segments[this.currentSegmentIndex].noteToggles[note.id - 1] = isPlaying;
+    if (isPlaying) {
+      //play frequency audio
+    }
+    return;
+  }
+
+  selectSegment(index: number) {
     this.currentSegmentIndex = index;
     this.playSegment(index);
     return;
   }
 
-  playSegment(segmentIndex: number){
-  	//play all note frequencies according to this.segments[segmentIndex].noteToggles
-  	// for a duration of this.segments[segmentIndex].duration according to defined BPM
+  playSegment(segmentIndex: number) {
+    //play all note frequencies according to this.segments[segmentIndex].noteToggles
+    // for a duration of this.segments[segmentIndex].duration according to defined BPM
     return segmentIndex;
   }
 
